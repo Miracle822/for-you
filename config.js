9c82ba40612da380847f83659835a7bcf6009a14
Miracle822 <amir1641043@gmail.com>
2026-08-21 15:09:17 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 // Change these lines. Then save the file.
 window.SITE = {
-  herName: "My Love",
-  yourName: "",
+  herName: "nazanin",
+  yourName: "alireza",
   message:
     "I made this little corner of the internet for your birthday. There is a song I wrote for you, and some photos of us. I hope it makes you smile.",
-  songFile: "audio/song.mp3",
+  songFile: "jess-fire-Different-Faces-But-The-Same-Old-Fight320.mp3",
   photos: [
-    download.jpg
+    "download.jpg"
     // Add your files like this after you put them in the photos folder:
     // "photos/us-1.jpg",
     // "photos/us-2.jpg",
