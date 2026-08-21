9cf50810f9fbaa17e48e623996b0b623dda8a8c2
Miracle822 <amir1641043@gmail.com>
2026-08-21 15:20:54 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Change these lines. Then save the file.
 window.SITE = {
   herName: "Nazanin",
-  yourName: "Alireza",
+  yourName: "Ali",
   message:
     "Tavalodet mobarak bashe nafasam omidavaram mano babat raftaram bebakhshi khoshgelam ino beddon ke dosset daram va ta akhare omr pa be patam in ahangam taghdim be ghashangiaye toe.",
   songFile: "audio/song.mp3",
