527d8e2dc6935216dea17ced1a87a1b6ac3d4a8d
Miracle822 <amir1641043@gmail.com>
2026-08-21 14:35:50 +0330

    Add files via upload

---
+++ for-you/config.js
@@ -0,0 +1,13 @@
+// Change these lines. Then save the file.
+window.SITE = {
+  herName: "My Love",
+  yourName: "",
+  message:
+    "I made this little corner of the internet for your birthday. There is a song I wrote for you, and some photos of us. I hope it makes you smile.",
+  songFile: "audio/song.mp3",
+  photos: [
+    // Add your files like this after you put them in the photos folder:
+    // "photos/us-1.jpg",
+    // "photos/us-2.jpg",
+  ],
+};
