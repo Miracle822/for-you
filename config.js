ced7f94e11cf1d218406cf03a9ad4d8ed44fd010
Miracle822 <amir1641043@gmail.com>
2026-08-21 15:08:18 +0330

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ window.SITE = {
     "I made this little corner of the internet for your birthday. There is a song I wrote for you, and some photos of us. I hope it makes you smile.",
   songFile: "audio/song.mp3",
   photos: [
+    download.jpg
     // Add your files like this after you put them in the photos folder:
     // "photos/us-1.jpg",
     // "photos/us-2.jpg",
